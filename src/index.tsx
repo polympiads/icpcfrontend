931804a014da1f33dbbf7f33d4fe7ef566e567f3
Eliott Tornassat <eliott.tornassat@epfl.ch>
2026-03-27 17:57:43 +0100

    fix merge conflict

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 /* @refresh reload */
-<<<<<<< HEAD
 import { render } from 'solid-js/web'
 import 'normalize.css'
 
@@ -8,14 +7,3 @@ import App from './App.tsx'
 const root = document.getElementById('root')
 
 render(() => <App />, root!)
-=======
-import { render } from "solid-js/web";
-import "normalize.css";
-
-import App from "./App.tsx";
-import "./index.css";
-
-const root = document.getElementById("root");
-
-render(() => <App />, root!);
->>>>>>> 32d8477 (initial commit)
