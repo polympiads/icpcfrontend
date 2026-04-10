251cd67d8dd7f47dfcdaec0062d651e01e9f001a
Eliott Tornassat <eliott.tornassat@epfl.ch>
2026-04-10 19:13:22 +0200

    implemented url logic for problem selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { FaRegularCalendarAlt, FaSolidMountain, FaSolidWarning } from "solid-icons/fa";
-import { Route, Router, useNavigate, useParams } from "@solidjs/router";
-import { PDFViewer } from "pdfslick"
+import {
+	FaRegularCalendarAlt,
+	FaSolidMountain,
+} from "solid-icons/fa";
+import { A, Route, Router, useNavigate, useParams } from "@solidjs/router";
 
 import { API_HOSTNAME } from "./constants";
 import { UserLoginWidget } from "./User";
@@ -10,22 +12,30 @@ import { ContestSelect } from "./Contest";
 import { FeedProvider } from "./worker/context/FeedContext";
 import dayjs, { duration } from "dayjs";
 import { Panel, SplitPanel } from "./SplitPanel";
-import { useStatement } from "./worker/hooks/useProblems";
-import { Button } from "@kobalte/core/button";
-import { Printer, RotateCw } from "lucide-solid";
-import { LoadingAnimation } from "./LoadingAnimation";
-import { createEffect, createSignal, onMount, type ParentProps } from "solid-js";
+import { ArrowLeft, ChevronDown, Printer } from "lucide-solid";
+import {
+	createEffect,
+	createSignal,
+	ErrorBoundary,
+	onMount,
+	Show,
+	type ParentProps,
+} from "solid-js";
 import { Tabs } from "@kobalte/core/tabs";
+import { Select } from "@kobalte/core/select";
+import type { Problem } from "./worker/types/data/Problems";
+import { useProblems } from "./worker/hooks/useProblems";
+import { BsExclamationCircle } from "solid-icons/bs";
 
-dayjs.extend(duration)
+dayjs.extend(duration);
 
 function ContestSelectionPage() {
 	return (
 		<div class="w-full h-full flex flex-col">
 			<div class="h-20 w-full p-3 border-b border-black/10 shadow-xl flex flex-row">
-				<FaSolidMountain class="h-14 w-14 aspect-square object-cover opacity-50"/>
-			
-				<div class="grow"/>
+				<FaSolidMountain class="h-14 w-14 aspect-square object-cover opacity-50" />
+
+				<div class="grow" />
 
 				<UserLoginWidget />
 			</div>
@@ -33,98 +43,221 @@ function ContestSelectionPage() {
 				<ContestSelect />
 			</div>
 		</div>
-	)
+	);
 }
 
 function InContestPage() {
-	const urlParams = useParams()
-	const navigate = useNavigate()
+	const urlParams = useParams();
+	const navigate = useNavigate();
 
-	const [selectedTab, setSelectedTab] = createSignal("problems")
-	
-	const TABS = ["problems", "scoreboard", "print"]
-	onMount(() => {
-		const wild = urlParams.rest
-		if (wild === undefined)
-			throw "That component should be used for the /contests/:id/* route."
-		
-		if (wild === "") {
-			navigate("./problems")
-		} else if (TABS.includes(wild)) {
-			setSelectedTab(wild)
-		} else {
-			navigate("/404")
+	const problems = useProblems();
+
+	const [selectedProblem, setSelectedProblem] = createSignal<Problem>(
+		Object.values(problems())[0],
+	);
+
+	function onProblemSelect(problem: Problem | null) {
+		if (!problem) {
+			return;
 		}
-	})
+
+		setSelectedProblem(problem);
+	}
+
+	const [selectedTab, setSelectedTab] = createSignal("problems");
+
+	const TABS = ["problems", "scoreboard", "print"];
+	onMount(() => {
+		const wild = urlParams.rest;
+		if (wild === undefined)
+			throw "That component should be used for the /contests/:id/* route.";
+
+		const wildParts = wild.split("/");
+		const segment = wildParts[0];
+		console.log(segment);
+
+		const firstProblem = Object.values(problems())[0]
+		if (firstProblem === undefined) {
+			throw "Error no problems available for that contest"
+		}
+
+		if (wild === "") {
+			setSelectedProblem(firstProblem);
+		} else if (TABS.includes(segment)) {
+			setSelectedTab(segment);
+
+			if (segment === "problems") {
+				if (wildParts.length > 1) {
+					const problem_id = wildParts[1];
+					if (problem_id in problems()) {
+						setSelectedProblem(problems()[problem_id]);
+					} else {
+						setSelectedProblem(firstProblem);
+					}
+				} else {
+					setSelectedProblem(firstProblem);
+				}
+			}
+		} else {
+			navigate("/404");
+		}
+	});
 	createEffect(() => {
-		navigate(`./${selectedTab()}`)
-	})
+		const selectedTabVal = selectedTab();
+
+		if (selectedTabVal !== "problems") {
+			navigate(`./${selectedTabVal}`, { replace: true });
+		} else {
+			const problem = selectedProblem();
+			navigate(`./${selectedTabVal}/${problem?.id}`, { replace: true });
+		}
+	});
+
+	function ProblemInfo(props: { problem: Problem }) {
+		return (
+			<>
+				P{props.problem.label} - {props.problem.name}
+			</>
+		);
+	}
 
 	return (
 		<div class="relative w-full h-full">
-			{/* <div class="absolute w-full h-full bg-white z-10">
-				<ContestPageOverlay contest={contest()}/>
-			</div> */}
-			
-			<div class="relative w-full h-full flex flex-col z-0">
-				<div class="relative h-20 w-full p-3 border-b border-black/10 shadow-xl flex flex-row z-10">
-					<FaSolidMountain class="h-14 w-14 aspect-square object-cover opacity-50"/>
-				
-					<div class="grow"/>
+				{/* <div class="absolute w-full h-full bg-white z-10">
+					<ContestPageOverlay contest={contest()}/>
+				</div> */}
 
-					<UserLoginWidget />
-				</div>
-				<div class="grow w-full overflow-hidden">
-					<Tabs class="h-full flex flex-col" value={selectedTab()} onChange={setSelectedTab}>
-						<Tabs.List class="*:bg-white *:border *:border-black/10 *:p-1 *:ui-highlighted:shadow-md *:opacity-75 *:ui-highlighted:opacity-100 *:ui-highlighted:z-10 *:ui-highlighted:hover:border-black/10 *:ui-highlighted:scale-105 *:ui-highlighted:cursor-auto *:cursor-pointer *:rounded-lg *:w-30 *:hover:opacity-100 *:hover:border-black/30 *:duration-75 px-2.5 pt-2 gap-2 flex flex-row">
-							<Tabs.Trigger value="problems"> Problems </Tabs.Trigger>
-							<Tabs.Trigger value="scoreboard" class="flex flex-row gap-1 justify-center items-center"> <FaRegularCalendarAlt size="1rem" class="float-left"/> Scoreboard </Tabs.Trigger>
-							<Tabs.Trigger value="print" class="flex flex-row gap-1 justify-center items-center"> <Printer size="1rem"/> Print </Tabs.Trigger>
-						</Tabs.List>
-						<Tabs.Content value="problems" class="grow w-full">
-							<SplitPanel direction="horizontal" class="h-full"includeMargin>
-								<Panel></Panel>
-								<SplitPanel direction="vertical">
+				<div class="relative w-full h-full flex flex-col z-0">
+					<div class="relative h-20 w-full p-3 border-b border-black/10 shadow-xl flex flex-row z-10">
+						<FaSolidMountain class="h-14 w-14 aspect-square object-cover opacity-50" />
+
+						<div class="grow" />
+
+						<UserLoginWidget />
+					</div>
+					<div class="grow w-full overflow-hidden">
+						<Tabs
+							class="h-full flex flex-col"
+							value={selectedTab()}
+							onChange={setSelectedTab}
+						>
+							<Tabs.List class="*:bg-white *:border *:border-black/10 *:p-1 *:ui-highlighted:shadow-md *:opacity-75 *:ui-highlighted:opacity-100 *:ui-highlighted:z-10 *:ui-highlighted:hover:border-black/10 *:ui-highlighted:scale-105 *:ui-highlighted:cursor-auto *:cursor-pointer *:rounded-lg *:min-w-30 *:hover:opacity-100 *:hover:border-black/30 *:duration-75 px-2.5 pt-2 gap-2 flex flex-row">
+								<Tabs.Trigger value="problems">
+									<Show when={problems() !== undefined}>
+										<Select
+											value={selectedProblem()}
+											onChange={onProblemSelect}
+											options={Object.values(problems())}
+											optionValue="id"
+											itemComponent={(props) => (
+												<Select.Item item={props.item}>
+													<Select.ItemLabel class="px-2 hover:bg-black/20 outline-t border-t border-black/10 max-w-80 leading-7.5 box-border">
+														<ProblemInfo problem={props.item.rawValue} />
+													</Select.ItemLabel>
+												</Select.Item>
+											)}
+										>
+											<div class="flex flex-row flex-nowrap items-center">
+												<Select.Trigger class="flex flex-row flex-nowrap items-center hover:bg-black/20 rounded-full border border-black/20 cursor-pointer">
+													<Select.Icon class="">
+														<ChevronDown size="1.4rem" />
+													</Select.Icon>
+												</Select.Trigger>
+												<div class="ml-1">
+													<Select.Value<Problem>>
+														{(state) => (
+															<div class="flex flex-row flex-nowrap">
+																{" "}
+																<ProblemInfo
+																	problem={state.selectedOption()}
+																/>{" "}
+															</div>
+														)}
+													</Select.Value>
+												</div>
+											</div>
+											<Select.Portal>
+												<Select.Content class="bg-white border border-black/20 rounded-md shadow-lg">
+													<div class="max-h-ui-popup-h max-w-ui-popup-w overflow-auto">
+														<Select.Listbox class="flex flex-col flex-wrap h-ui-popup-h" />
+													</div>
+												</Select.Content>
+											</Select.Portal>
+										</Select>
+									</Show>
+								</Tabs.Trigger>
+								<Tabs.Trigger
+									value="scoreboard"
+									class="flex flex-row gap-1 justify-center items-center"
+								>
+									{" "}
+									<FaRegularCalendarAlt size="1rem" class="float-left" />{" "}
+									Scoreboard{" "}
+								</Tabs.Trigger>
+								<Tabs.Trigger
+									value="print"
+									class="flex flex-row gap-1 justify-center items-center"
+								>
+									{" "}
+									<Printer size="1rem" /> Print{" "}
+								</Tabs.Trigger>
+							</Tabs.List>
+							<Tabs.Content value="problems" class="grow w-full">
+								<SplitPanel direction="horizontal" class="h-full" includeMargin>
 									<Panel></Panel>
-									<Panel></Panel>
+									<SplitPanel direction="vertical">
+										<Panel></Panel>
+										<Panel></Panel>
+									</SplitPanel>
 								</SplitPanel>
-							</SplitPanel>
-						</Tabs.Content>
-						<Tabs.Content value="scoreboard">
-							Scoreboard
-						</Tabs.Content>
-						<Tabs.Content value="print">
-							Print
-						</Tabs.Content>
-					</Tabs>
+							</Tabs.Content>
+							<Tabs.Content value="scoreboard">Scoreboard</Tabs.Content>
+							<Tabs.Content value="print">Print</Tabs.Content>
+						</Tabs>
+					</div>
 				</div>
-			</div>
 		</div>
+	);
+}
+
+function PageCrashHandler(props: ParentProps) {
+	return (
+		<ErrorBoundary fallback={
+			<div class="w-full h-full flex flex-col items-center justify-center">
+				<BsExclamationCircle size="3em" />
+				<div class="text-xl font-medium mb-3"> Something went wrong. </div>
+				<A href="/">
+					<div class="border border-black/10 p-2 rounded-md flex flex-row flex-nowrap items-center group/return_button overflow-hidden cursor-pointer">
+						<ArrowLeft size="1rem" />
+						<div class="w-0 group-hover/return_button:w-30 text-nowrap duration-75">
+							<div class="ml-2">Return to menu</div>
+						</div>
+					</div>
+				</A>
+			</div>
+		}>
+			{ props.children }
+		</ErrorBoundary>
 	)
 }
 
 function RouteFeedWrapper(props: ParentProps) {
 	const urlParams = useParams();
 	if (!urlParams.id) {
-		throw "This component should be used for the /contests/:id route."
+		throw "This component should be used for the /contests/:id route.";
 	}
 
-	return (
-		<FeedProvider contestId={ urlParams.id }>
-			{ props.children }
-		</FeedProvider>
-	)
+	return <FeedProvider contestId={urlParams.id}>{props.children}</FeedProvider>;
 }
-
 
 function App() {
 	return (
 		<WorkerProvider apiHostname={API_HOSTNAME}>
 			<AuthProvider>
 				<Router>
-					<Route path="/" component={ContestSelectionPage}/>
+					<Route path="/" component={ContestSelectionPage} />
 					<Route path="/contests/:id" component={RouteFeedWrapper}>
-						<Route path="/*rest" component={InContestPage}/>
+						<Route path="/*rest" component={() => <PageCrashHandler><InContestPage/></PageCrashHandler>} />
 					</Route>
 				</Router>
 			</AuthProvider>
